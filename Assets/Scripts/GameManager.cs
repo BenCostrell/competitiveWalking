1ba7f8d0f8ff35cadd9dfa28d0d40490d8004f88
Ben Costrell <bencostrell@gmail.com>
2017-02-08 14:53:07 -0500

    basic movement/rotation working, controller drift issues addressed
    man that was a pain in the ass

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject playerPrefab;
+	public GameObject player1;
+	public GameObject player2;
+	public Vector3 player1Spawn;
+	public Vector3 player2Spawn;
+
+	// Use this for initialization
+	void Start () {
+		InitializePlayers ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Reset")){
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+
+	void InitializePlayers(){
+
+
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	private CharacterController characterController;
+	public int playerNum;
+	public float speed;
+	public float lookDistance;
+	public float rotationSpeed;
+	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+		characterController = GetComponent<CharacterController> ();
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Look ();
 	}
+
+	void FixedUpdate(){
+		Move ();
+		RotateView ();
+	}
+
+	void Move(){
+		Vector3 inputDirection = new Vector3 (Input.GetAxis ("Horizontal_P" + playerNum), 0, Input.GetAxis ("Vertical_P" + playerNum));
+		Vector3 movementDirection = Quaternion.Euler (transform.rotation.eulerAngles) * inputDirection;
+		rb.velocity = speed * movementDirection;
+	}
+
+	void RotateView(){
+		float input;
+		input = Input.GetAxis("Rotation_P" + playerNum);
+		if (input != 0) {
+			transform.Rotate (Vector3.up, rotationSpeed * Mathf.Sign (input));
+		}
+	}
+
+	void Look(){
+		float angleLooking = Mathf.Deg2Rad * transform.rotation.eulerAngles.y;
+		Vector3 lookDirection = new Vector3 (Mathf.Cos (angleLooking), 0, Mathf.Sin (angleLooking));
+		Debug.DrawRay (transform.position, transform.position + lookDirection);
+		if (Physics.Raycast (transform.position, transform.position + lookDirection, lookDistance)) {
+			Debug.Log ("saw a thing");
+		}
+	}
+
 }
